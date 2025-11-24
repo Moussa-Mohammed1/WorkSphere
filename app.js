5c6b3d5953da7904876103b4be8c1ef8dcc35cd8
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2025-11-24 10:03:27 +0100

    fix : fix image url input loaded after last filled form

--- app.js
+++ app.js
@@ -283,6 +283,7 @@ function clearForm(){
     staffPhone.value = "";
     staffEmail.value = "";
     profilImg.src = "https://i.pinimg.com/1200x/6e/59/95/6e599501252c23bcf02658617b29c894.jpg";
+    imgUrl.value = "";
     for (let i = 0; i < expArray.length; i++) {
         const item = expArray[i];
         item.querySelector('[name="exp-title"]').value = "";
